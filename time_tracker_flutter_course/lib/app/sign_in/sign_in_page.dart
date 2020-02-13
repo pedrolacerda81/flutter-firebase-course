4764d7d1bf08cd26e79b51c5bbe0f7f0d06b88de
Pedro - GitPro <peidom81@gmail.com>
2020-02-13 19:39:25 -0400

    Aula 96

--- time_tracker_flutter_course/lib/app/sign_in/sign_in_page.dart
+++ time_tracker_flutter_course/lib/app/sign_in/sign_in_page.dart
@@ -4,8 +4,37 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Time Tracker'),
+      appBar: AppBar(title: Text('Time Tracker'), elevation: 2.0),
+      body: _buildBodyContent(),
+    );
+  }
+
+  Padding _buildBodyContent() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            child: Text(
+              'Sign In',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 32.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          SizedBox(height: 8.0),
+          RaisedButton(
+            child: Text(
+              'Sign in with Google',
+            ),
+            onPressed: () {},
+          ),
+        ],
       ),
     );
   }
