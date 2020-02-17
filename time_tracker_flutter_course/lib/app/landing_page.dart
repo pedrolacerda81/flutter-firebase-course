54b112c7e051eb51925daeed108b1f2f13722263
Pedro - GitPro <peidom81@gmail.com>
2020-02-17 15:38:38 -0400

    Changes the global access to dependency injection

--- time_tracker_flutter_course/lib/app/landing_page.dart
+++ time_tracker_flutter_course/lib/app/landing_page.dart
@@ -1,18 +1,30 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/home/home_page.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
 
 class LandingPage extends StatefulWidget {
+  LandingPage({@required this.auth});
+  final AuthBase auth;
   @override
   _LandingPageState createState() => _LandingPageState();
 }
 
 class _LandingPageState extends State<LandingPage> {
-  FirebaseUser _user;
+  User _user;
 
-  void _updateUser(FirebaseUser user) {
-    print('User id: ${user?.uid}');
+  @override
+  void initState() {
+    super.initState();
+    _checkCurrentUser();
+  }
+
+  Future<void> _checkCurrentUser() async {
+    User user = await widget.auth.currentUser();
+    _updateUser(user);
+  }
+
+  void _updateUser(User user) {
     setState(() {
       _user = user;
     });
@@ -22,11 +34,13 @@ class _LandingPageState extends State<LandingPage> {
   Widget build(BuildContext context) {
     if (_user == null) {
       return SignInPage(
+        auth: widget.auth,
         onSignIn:
             _updateUser, //don't need to pass FirebaseUser user because it's required on both
       );
     }
     return HomePage(
+      auth: widget.auth,
       onSignOut: () => _updateUser(null),
     );
   }
