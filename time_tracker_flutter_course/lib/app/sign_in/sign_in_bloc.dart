d4e811767ed5a65eee7d663c88b0fd8453cdb95a
Pedro - GitPro <peidom81@gmail.com>
2020-02-28 19:40:25 -0400

    SignInBloc wrapup

--- time_tracker_flutter_course/lib/app/sign_in/sign_in_bloc.dart
+++ time_tracker_flutter_course/lib/app/sign_in/sign_in_bloc.dart
@@ -1,10 +1,28 @@
 import 'dart:async';
+import 'package:flutter/foundation.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
 
 class SignInBloc {
+  SignInBloc({@required this.auth});
+
+  final AuthBase auth;
   final StreamController<bool> _isLoadingController = StreamController<bool>();
   Stream<bool> get isLoadingStream => _isLoadingController.stream;
 
-  void setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
+  void _setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
+
+  Future<User> _signIn(Future<User> Function() signInMethod) async {
+    try {
+      _setIsLoading(true);
+      return await signInMethod();
+    } catch (e) {
+      _setIsLoading(false);
+      rethrow;
+    }
+  }
+
+  Future<User> signInAnonymously() async => await _signIn(auth.signInAnonymously);
+  Future<User> signInWithGoogle() async => await _signIn(auth.signInWithGoogle);
 
   void dispose() {
     _isLoadingController.close();
--- time_tracker_flutter_course/lib/app/sign_in/sign_in_page.dart
+++ time_tracker_flutter_course/lib/app/sign_in/sign_in_page.dart
@@ -12,10 +12,11 @@ class SignInPage extends StatelessWidget {
   SignInPage({Key key, @required this.bloc}) : super(key: key);
 
   final SignInBloc bloc;
-  
+
   static Widget create(BuildContext context) {
+    final auth = Provider.of<AuthBase>(context);
     return Provider<SignInBloc>(
-      create: (_) => SignInBloc(),
+      create: (_) => SignInBloc(auth: auth),
       dispose: (context, bloc) => bloc.dispose(),
       child: Consumer<SignInBloc>(
         builder: (context, bloc, _) => SignInPage(
@@ -34,27 +35,19 @@ class SignInPage extends StatelessWidget {
 
   Future<void> _signInAnonymously(BuildContext context) async {
     try {
-      bloc.setIsLoading(true);
-      final auth = Provider.of<AuthBase>(context, listen: false);
-      await auth.signInAnonymously();
+      await bloc.signInAnonymously();
     } on PlatformException catch (e) {
       _showSignInError(context, e);
-    } finally {
-      bloc.setIsLoading(false);
     }
   }
 
   Future<void> _signInWithGoogle(BuildContext context) async {
     try {
-      bloc.setIsLoading(true);
-      final auth = Provider.of<AuthBase>(context, listen: false);
-      await auth.signInWithGoogle();
+      await bloc.signInWithGoogle();
     } on PlatformException catch (e) {
       if (e.code != 'ERROR_ABORTED_BY_USER') {
         _showSignInError(context, e);
       }
-    } finally {
-      bloc.setIsLoading(false);
     }
   }
 
