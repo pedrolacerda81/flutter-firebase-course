134ba23d4462f61968aaf4f72d4e2d23be3ef182
Pedro - GitPro <peidom81@gmail.com>
2020-03-06 15:19:59 -0400

    Prepares to deal with entries from Firebase

--- time_tracker_flutter_course/lib/services/firestore_service.dart
+++ time_tracker_flutter_course/lib/services/firestore_service.dart
@@ -1,11 +1,14 @@
-import 'package:meta/meta.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:meta/meta.dart';
 
 class FirestoreService {
   FirestoreService._();
   static final instance = FirestoreService._();
 
-  Future<void> setData({@required String path, @required Map<String, dynamic> data}) async {
+  Future<void> setData({
+    @required String path,
+    @required Map<String, dynamic> data,
+  }) async {
     final reference = Firestore.instance.document(path);
     print('$path: $data');
     await reference.setData(data);
@@ -17,14 +20,35 @@ class FirestoreService {
     await reference.delete();
   }
 
-  Stream<List<T>> collectionStream<T>(
-      {@required String path, @required T builder(Map<String, dynamic> data, String documentId)}) {
-    final reference = Firestore.instance.collection(path);
-    final snapshots = reference.snapshots();
-    return snapshots.map((snapshot) => snapshot.documents
-        .map(
-          (snapshot) => builder(snapshot.data, snapshot.documentID),
-    )
-        .toList());
+  Stream<List<T>> collectionStream<T>({
+    @required String path,
+    @required T builder(Map<String, dynamic> data, String documentID),
+    Query queryBuilder(Query query),
+    int sort(T lhs, T rhs),
+  }) {
+    Query query = Firestore.instance.collection(path);
+    if (queryBuilder != null) {
+      query = queryBuilder(query);
+    }
+    final Stream<QuerySnapshot> snapshots = query.snapshots();
+    return snapshots.map((snapshot) {
+      final result = snapshot.documents
+          .map((snapshot) => builder(snapshot.data, snapshot.documentID))
+          .where((value) => value != null)
+          .toList();
+      if (sort != null) {
+        result.sort(sort);
+      }
+      return result;
+    });
+  }
+
+  Stream<T> documentStream<T>({
+    @required String path,
+    @required T builder(Map<String, dynamic> data, String documentID),
+  }) {
+    final DocumentReference reference = Firestore.instance.document(path);
+    final Stream<DocumentSnapshot> snapshots = reference.snapshots();
+    return snapshots.map((snapshot) => builder(snapshot.data, snapshot.documentID));
   }
 }
