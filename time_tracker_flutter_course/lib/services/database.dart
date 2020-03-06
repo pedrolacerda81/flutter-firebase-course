1cf7b63f7019489fb9112c6a9c5c09a5d8edcaaf
Pedro - GitPro <peidom81@gmail.com>
2020-03-06 18:22:20 -0400

    Bug Fix on Editing Job

--- time_tracker_flutter_course/lib/services/database.dart
+++ time_tracker_flutter_course/lib/services/database.dart
@@ -10,6 +10,7 @@ abstract class Database {
   Future<void> setJob(Job job);
   Future<void> deleteJob(Job job);
   Stream<List<Job>> jobsStream();
+  Stream<Job> jobStream({@required String jobId});
 
   Future<void> setEntry(Entry entry);
   Future<void> deleteEntry(Entry entry);
@@ -44,6 +45,11 @@ class FirestoreDatabase implements Database {
   }
 
   @override
+  Stream<Job> jobStream({@required String jobId}) => _service.documentStream(
+      path: APIPath.job(uid, jobId),
+      builder: (data, documentId) => Job.fromMap(data, documentId));
+
+  @override
   Stream<List<Job>> jobsStream() => _service.collectionStream(
         path: APIPath.jobs(uid),
         builder: (data, documentId) => Job.fromMap(data, documentId),
@@ -51,18 +57,22 @@ class FirestoreDatabase implements Database {
 
   @override
   Future<void> setEntry(Entry entry) async => await _service.setData(
-    path: APIPath.entry(uid, entry.id),
-    data: entry.toMap(),
-  );
+        path: APIPath.entry(uid, entry.id),
+        data: entry.toMap(),
+      );
 
   @override
-  Future<void> deleteEntry(Entry entry) async => await _service.deleteData(path: APIPath.entry(uid, entry.id));
+  Future<void> deleteEntry(Entry entry) async =>
+      await _service.deleteData(path: APIPath.entry(uid, entry.id));
 
   @override
-  Stream<List<Entry>> entriesStream({Job job}) => _service.collectionStream<Entry>(
-    path: APIPath.entries(uid),
-    queryBuilder: job != null ? (query) => query.where('jobId', isEqualTo: job.id) : null,
-    builder: (data, documentID) => Entry.fromMap(data, documentID),
-    sort: (lhs, rhs) => rhs.start.compareTo(lhs.start),
-  );
+  Stream<List<Entry>> entriesStream({Job job}) =>
+      _service.collectionStream<Entry>(
+        path: APIPath.entries(uid),
+        queryBuilder: job != null
+            ? (query) => query.where('jobId', isEqualTo: job.id)
+            : null,
+        builder: (data, documentID) => Entry.fromMap(data, documentID),
+        sort: (lhs, rhs) => rhs.start.compareTo(lhs.start),
+      );
 }
