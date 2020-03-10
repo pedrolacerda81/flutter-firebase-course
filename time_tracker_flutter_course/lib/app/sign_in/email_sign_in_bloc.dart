ad824b3b1501bd23a64ebc2d4a79091867229c2f
Pedro - GitPro <peidom81@gmail.com>
2020-03-10 16:49:48 -0400

    Adds a behaviorSubject to the emailSignInBloc

--- time_tracker_flutter_course/lib/app/sign_in/email_sign_in_bloc.dart
+++ time_tracker_flutter_course/lib/app/sign_in/email_sign_in_bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/foundation.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/email_sign_in_model.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
@@ -7,14 +8,14 @@ class EmailSignInBloc {
   final AuthBase auth;
   EmailSignInBloc({@required this.auth});
 
-  final StreamController<EmailSignInModel> _modelController =
-      StreamController<EmailSignInModel>();
+  final _modelSubject =
+      BehaviorSubject<EmailSignInModel>.seeded(EmailSignInModel());
 
-  Stream<EmailSignInModel> get modelStream => _modelController.stream;
-  EmailSignInModel _model = EmailSignInModel();
+  Stream<EmailSignInModel> get modelStream => _modelSubject.stream;
+  EmailSignInModel get _model => _modelSubject.value;
 
   void dispose() {
-    _modelController.close();
+    _modelSubject.close();
   }
 
   void toggleFormType() {
@@ -40,13 +41,13 @@ class EmailSignInBloc {
       EmailSignInFormType formType,
       bool isLoading,
       bool submitted}) {
-    _model.copyWith(
-        email: email,
-        password: password,
-        emailSignInFormType: formType,
-        isLoading: isLoading,
-        submitted: submitted);
-    _modelController.add(_model);
+    _modelSubject.value = _model.copyWith(
+      email: email,
+      password: password,
+      emailSignInFormType: formType,
+      isLoading: isLoading,
+      submitted: submitted,
+    );
   }
 
   Future<void> submit() async {
