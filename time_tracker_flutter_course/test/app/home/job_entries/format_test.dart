73d797a073bc456ab975f3460b034eec5c8b8362
Pedro - GitPro <peidom81@gmail.com>
2020-03-12 14:43:26 -0400

    Adds Currency tests at Format class

--- time_tracker_flutter_course/test/app/home/job_entries/format_test.dart
+++ time_tracker_flutter_course/test/app/home/job_entries/format_test.dart
@@ -54,4 +54,42 @@ void main() {
       expect(Format.date(DateTime(2020, 2, 28)), '28 de fev de 2020');
     });
   });
+
+  group('Day of the week - GB Locale', () {
+    setUp(() async {
+      Intl.defaultLocale = 'en_GB';
+      await initializeDateFormatting(Intl.defaultLocale);
+    });
+    test('Frieday', () {
+      expect(Format.dayOfWeek(DateTime(2020, 3, 13)), 'Fri');
+    });
+  });
+
+  group('Day of the week - pt_BR Locale', () {
+    setUp(() async {
+      Intl.defaultLocale = 'pt_BR';
+      await initializeDateFormatting(Intl.defaultLocale);
+    });
+    test('Sexta-feira', () {
+      expect(Format.dayOfWeek(DateTime(2020, 3, 13)), 'sex');
+    });
+  });
+
+  group('Currency - US Locale', () {
+    setUp(() async {
+      Intl.defaultLocale = 'en_US';
+      await initializeDateFormatting(Intl.defaultLocale);
+    });
+    test('positive', () {
+      expect(Format.currency(10), '\$10');
+    });
+
+    test('zero', () {
+      expect(Format.currency(0), '');
+    });
+
+    test('negative', () {
+      expect(Format.currency(-5), '-\$5');
+    });
+  });
 }
