4ddd2a55246e5a32c77d74230250d1e801a5ac6e
Pedro - GitPro <peidom81@gmail.com>
2020-03-12 13:47:07 -0400

    Adds more tests on validators and fix validators code

--- time_tracker_flutter_course/test/app/sign_in/validators_test.dart
+++ time_tracker_flutter_course/test/app/sign_in/validators_test.dart
@@ -2,8 +2,18 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/validators.dart';
 
 void main() {
-  test('Validator non empty String', () {
+  test('Validator -  non empty String', () {
     final validator = NonEmptyStringValidator();
     expect(validator.isValid('test'), true);
+  });
+
+  test('Validator - empty String', () {
+    final validator = NonEmptyStringValidator();
+    expect(validator.isValid(''), false);
+  });
+
+  test('Validator - null String', () {
+    final validator = NonEmptyStringValidator();
+    expect(validator.isValid(null), false);
   });
 }
